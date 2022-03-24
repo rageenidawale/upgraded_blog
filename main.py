1ecea7090982140ac4c76e2697e7943efa091955
Rageeni Dawale <rageenidawale@gmail.com>
2022-03-24 12:54:25 +0530

    Add contact page functionality

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+
+MY_EMAIL = ""
+PASSWORD = ""
 
 posts = requests.get("https://api.npoint.io/710254173db50d6799fb").json()
 
@@ -26,9 +30,24 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    is_message_sent = False
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        message = request.form.get("message")
+        is_message_sent = True
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=MY_EMAIL,
+                msg=f"Subject:New Message\n\nName: {name} \nEmail: {email} \nPhone: {phone} \nMessage: {message}"
+            )
+    return render_template("contact.html", is_message_sent=is_message_sent)
 
 
 if __name__ == "__main__":
